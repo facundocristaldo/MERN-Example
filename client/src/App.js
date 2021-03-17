3f60f58c99bce7a39b1f1dd253678a0b8fd12519
Facundo <facucristaldorodriguez@gmail.com>
2021-03-17 11:19:08 -0300

    starting frontend with react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+
+const App = () => (
+  <>
+    <Navbar />
+    <Landing />
+    <h1>App</h1>
+  </>
+);
+
+export default App;
